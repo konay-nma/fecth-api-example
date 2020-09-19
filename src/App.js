be31f0664a2066207df6e71f55fc93b988e2a331
konay <mr.mr.naymyoaung@gmail.com>
2020-09-19 19:47:13 +0630

    initail comit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const url = 'https://us-central1-test-api-f282b.cloudfunctions.net/app'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    message: '',
+    isLoaded: false,
+    error: null,
+    items: [],
+    name: '',
+    price: ''
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`${url}/items`)
+      const result = await res.json()
+      this.setState({
+        isLoaded: true,
+        items: result
+      })
+    } catch (error) {
+      this.setState({
+        isLoaded: true,
+        error
+      })
+    }
+
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(result => {
+    //   this.setState({
+    //     items: result
+    //   }) 
+    // }, err=> {
+    //   this.setState({
+    //     err
+    //   })
+    // })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, price, items } = this.state
+    fetch(`${url}/add`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ name, price })
+    })
+      .then(res => res.json())
+      .then(result => {
+        (name && price) &&
+          this.setState({
+            items: [{ name, price }, ...items],
+            message: result.message
+          })
+
+          this.setState({
+            message : result.message
+          })
+      })
+      .catch(error => {
+        this.setState({
+          message: error.message
+        })
+      })
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    const { name, price, error, items, isLoaded, message } = this.state
+    if (error) {
+      return (
+        <div>
+          {error.message}
+        </div>
+      )
+    } else if (!isLoaded) {
+      return <div>
+        Loading....
+      </div>
+    } else {
+      return (
+        <div>
+          { items.length > 0 &&
+            <ul>
+              {items.map(item => <li key={item.id} >{item.name} ${item.price}</li>)}
+            </ul>
+          }
+          <form onSubmit={this.handleSubmit} >
+            <label>
+              Name : <span>&nbsp;</span>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="name"
+                value={name}
+              />
+            </label>
+            <span>&nbsp;&nbsp;</span>
+            <label>
+              Price : <span>&nbsp;</span>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="price"
+                value={price}
+              />
+            </label>
+            <span>&nbsp;&nbsp;</span>
+            <input
+              onChange={this.handleChange}
+              type='submit'
+              value="Add" />
+          </form>
+          {message &&
+            <div>{message}</div>
+          }
+        </div>
+      )
+    };
+  }
 }
-
 export default App;
